6514b9c7e8069bf31f85335ac4bba051edc29d44
W1nson <winsonchen108@gmail.com>
2026-02-25 14:05:38 -0600

    update

--- infant/config.py
+++ infant/config.py
@@ -123,7 +123,7 @@ class Config:
     gift_key: bool = False
     
     ## vllm Attributes (OSS-LLM)
-    model_oss: str = 'ByteDance-Seed/UI-TARS-1.5-7B'
+    model_oss: str = 'openai/ByteDance-Seed/UI-TARS-1.5-7B'
     api_key_oss: str | None = None
     base_url_oss: str | None = None
     tensor_parallel_size: int = 1 # Tensor parallelism splits the model's tensors across n GPUs
